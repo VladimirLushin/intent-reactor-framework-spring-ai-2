3de7790c43e8e94d2ea1ddca6a6ba7c6de7828a7
Лушин Владимир <lushago123@gmail.com>
2026-09-02 16:54:13 +0300

    Upgrade to Spring Boot 4.0.8, Spring AI 2.0.1, Java 21 target

--- intent-reactor-core/src/main/java/com/intentreactor/core/config/IntentReactorAutoConfiguration.java
+++ intent-reactor-core/src/main/java/com/intentreactor/core/config/IntentReactorAutoConfiguration.java
@@ -42,7 +42,7 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.boot.web.client.RestClientCustomizer;
+import org.springframework.boot.restclient.RestClientCustomizer;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
